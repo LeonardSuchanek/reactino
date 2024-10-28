2442915f076dbe6d2a8141bd2aade23320ac3b0c
Leonard Suchanek <leonard.suchanek23091999@gmail.com>
2024-10-28 21:32:00 +0100

    removed unused import

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Detail from './pages/Detail';
 
 const AppRouter = () => (
         <Routes>
