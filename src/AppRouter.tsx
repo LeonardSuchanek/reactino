2c38bcbff9fd51c81c6b438f5a1b82b6aaeac4d7
Leonard Suchanek <leonard.suchanek23091999@gmail.com>
2024-10-28 21:24:41 +0100

    app router config

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Detail from './pages/Detail';
 
 const AppRouter = () => (
-    <Router>
         <Routes>
             <Route path="/" element={<Home />} />
         </Routes>
-    </Router>
 );
 
 export default AppRouter;
