5b8c8a78864f9e3b81be06573ab1546618b8c6d3
halu1031 <lukas.haeffele@icloud.com>
2024-11-11 11:11:29 +0100

    Und ab damit

--- src/components/lessons/lesson1/L1C1.tsx
+++ src/components/lessons/lesson1/L1C1.tsx
@@ -1,8 +1,35 @@
 import CodeBox from "../../CodeBox";
+import { render } from "react-dom";
+import AceEditor from "react-ace";
+
 
 const L1C1 = () => {
   return (
-    <></>  );
+    <>
+      <AceEditor
+        placeholder="Placeholder Text"
+        mode="javascript"
+        theme="monokai"
+        name="blah2"
+        onLoad={this.onLoad}
+        onChange={this.onChange}
+        fontSize={14}
+        lineHeight={19}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={`function onLoad(editor) {
+  console.log("i've loaded");
+}`}
+        setOptions={{
+          enableBasicAutocompletion: false,
+          enableLiveAutocompletion: false,
+          enableSnippets: false,
+          enableMobileMenu: true,
+          showLineNumbers: true,
+          tabSize: 2,
+        }} />
+    </>);
 };
 
 export default L1C1;
