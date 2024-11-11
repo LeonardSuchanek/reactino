9fe87cf1104faf0bcffd88b0254dd6479bd9ad52
halu1031 <lukas.haeffele@icloud.com>
2024-11-11 11:54:05 +0100

    Aus mit dem Ace

--- src/components/lessons/lesson1/L1C1.tsx
+++ src/components/lessons/lesson1/L1C1.tsx
@@ -1,12 +1,5 @@
-import AceEditor from "react-ace";
 import { useState } from "react";
 
-// Importiere die gewünschten Sprach- und Theme-Dateien
-import "ace-builds/src-noconflict/mode-javascript";
-import "ace-builds/src-noconflict/theme-monokai";
-
-// Optional: Autocomplete und Snippets aktivieren
-import "ace-builds/src-noconflict/ext-language_tools";
 
 const L1C1 = () => {
   const [code, setCode] = useState<string>("// Start writing code here!");
@@ -20,24 +13,6 @@ const L1C1 = () => {
       <h1 className="font-bold text-4xl mb-12">
         Einführung in React-Komponenten
       </h1>
-      <AceEditor
-        mode="javascript" // Spracheinstellung
-        theme="monokai" // Theme
-        name="editor" // Einzigartiger Name für Editor-Instanz
-        fontSize={16}
-        width="100%"
-        height="400px"
-        value={code}
-        onChange={handleCodeChange} // Update-Funktion bei Codeänderung
-        editorProps={{ $blockScrolling: true }}
-        setOptions={{
-          enableBasicAutocompletion: true, // Autovervollständigung
-          enableLiveAutocompletion: true,
-          enableSnippets: true,
-          showLineNumbers: true,
-          tabSize: 2,
-        }}
-      />
     </div>
   );
 };
