beb56a4082e41cc0300f2bd39cde50402273f1ff
Lukas <79853178+halu1031@users.noreply.github.com>
2024-11-28 15:08:48 +0100

    Styling der Karten und Aufgaben

--- src/components/lessons/lesson1/L1C2.tsx
+++ src/components/lessons/lesson1/L1C2.tsx
@@ -8,141 +8,140 @@ import Button from "../../Button";
 import { Link } from "react-router-dom";
 
 const L1C2 = () => {
-  const items = [
-    { id: uuid(), content: "function", color: "bg-red-100" },
-    { id: uuid(), content: "ProjectsPage()", color: "bg-pink-100" },
-    { id: uuid(), content: "{", color: "bg-yellow-100" },
-    { id: uuid(), content: "}", color: "bg-yellow-100" },
-    { id: uuid(), content: "</h1>", color: "bg-red-100" },
-    { id: uuid(), content: "<h1>", color: "bg-red-100" },
-    { id: uuid(), content: "return", color: "bg-red-100" },
-    { id: uuid(), content: "Hello World", color: "bg-red-100" },
-    { id: uuid(), content: ";", color: "bg-green-100" },
-  ];
+    const items = [
+        { id: uuid(), content: "function", color: "bg-white" },
+        { id: uuid(), content: "ProjectsPage()", color: "bg-white" },
+        { id: uuid(), content: "{", color: "bg-white" },
+        { id: uuid(), content: "}", color: "bg-white" },
+        { id: uuid(), content: "</h1>", color: "bg-white" },
+        { id: uuid(), content: "<h1>", color: "bg-white" },
+        { id: uuid(), content: "return", color: "bg-white" },
+        { id: uuid(), content: "Hello World", color: "bg-white" },
+        { id: uuid(), content: ";", color: "bg-white" },
+    ];
 
-  const rowsBackend = {
-    Question: {
-      items: items,
-    },
-    Answer: {
-      items: [],
-    },
-  };
+    const rowsBackend = {
+        Question: {
+            items: items,
+        },
+        Answer: {
+            items: [],
+        },
+    };
 
-  const [rows, setRows] = useState(rowsBackend);
+    const [rows, setRows] = useState(rowsBackend);
 
-  const [winReady, setwinReady] = useState(false);
-  useEffect(() => {
-    setwinReady(true);
-  }, []);
+    const [winReady, setwinReady] = useState(false);
+    useEffect(() => {
+        setwinReady(true);
+    }, []);
 
-  return (
-    <main>
-      <DragDropContext
-        onDragEnd={(result) => services.onDragEnd(result, rows, setRows)}
-      >
-        <div>
-          <h1 className="font-bold text-4xl mb-12">
-            Bringe die Bauteile einer Komponente in die richtige Reihenfolge.
-          </h1>
-          <p>
-            Ziehe dafür die Bauteile in der richtigen Reigenfolge auf die Linie.
-            Zum Prüfen deiner Antwort nutze den Button.
-          </p>
-          <div className="answer-wrapper mt-10">
-            {winReady ? (
-              <Droppable droppableId="Answer" direction="horizontal">
-                {(provided, snapshot) => {
-                  return (
-                    <div
-                      className="answers flex gap-2 w-full h-10"
-                      ref={provided.innerRef}
-                      {...provided.droppableProps}
-                    >
-                      {rows["Answer"].items.map((item, index) => {
-                        return (
-                          <Draggable
-                            key={item}
-                            draggableId={item}
-                            index={index}
-                          >
-                            {(provided, snapshot) => {
-                              return <Option item={item} provided={provided} />;
-                            }}
-                          </Draggable>
-                        );
-                      })}
-                      {provided.placeholder}
+    return (
+        <main className="max-w-7xl md:justify-self-center ">
+            <DragDropContext
+                onDragEnd={(result) => services.onDragEnd(result, rows, setRows)}
+            >
+                <div>
+                    <h1 className="font-bold text-4xl md:text-5xl mb-1">
+                        Bringe die Bauteile einer Komponente in die richtige Reihenfolge.
+                    </h1>
+                    <p className="mb-8 text-xl md:text-2xl">
+                        Ziehe dafür die Bauteile in der richtigen Reigenfolge auf die Linie.
+                        Zum Prüfen deiner Antwort nutze den Button.
+                    </p>
+                    <div className="answer-wrapper mt-10">
+                        {winReady ? (
+                            <Droppable droppableId="Answer" direction="horizontal">
+                                {(provided, snapshot) => {
+                                    return (
+                                        <div
+                                            className="answers flex gap-2 w-full h-10"
+                                            ref={provided.innerRef}
+                                            {...provided.droppableProps}
+                                        >
+                                            {rows["Answer"].items.map((item, index) => {
+                                                return (
+                                                    <Draggable
+                                                        key={item}
+                                                        draggableId={item}
+                                                        index={index}
+                                                    >
+                                                        {(provided, snapshot) => {
+                                                            return <Option item={item} provided={provided} />;
+                                                        }}
+                                                    </Draggable>
+                                                );
+                                            })}
+                                            {provided.placeholder}
+                                        </div>
+                                    );
+                                }}
+                            </Droppable>
+                        ) : null}
+
+                        <hr className=" border-gray-600" />
+                        <br />
                     </div>
-                  );
-                }}
-              </Droppable>
-            ) : null}
+                    {winReady ? (
+                        <Droppable droppableId="Question" direction="horizontal">
+                            {(provided, snapshot) => {
+                                return (
+                                    <div
+                                        className="Question option-wrapper mt-16 flex items-start gap-2 h-28"
+                                        ref={provided.innerRef}
+                                        {...provided.droppableProps}
+                                    >
+                                        {rows["Question"].items.map((item, index) => {
+                                            return (
+                                                <Draggable
+                                                    key={item.id}
+                                                    draggableId={item.id}
+                                                    index={index}
+                                                >
+                                                    {(provided, snapshot) => {
+                                                        return <Option item={item} provided={provided} />;
+                                                    }}
+                                                </Draggable>
+                                            );
+                                        })}
+                                        {provided.placeholder}
+                                    </div>
+                                );
+                            }}
+                        </Droppable>
+                    ) : null}
 
-            <hr className=" border-gray-600" />
-            <br />
-          </div>
-          {winReady ? (
-            <Droppable droppableId="Question" direction="horizontal">
-              {(provided, snapshot) => {
-                return (
-                  <div
-                    className="Question option-wrapper mt-16 flex items-start gap-2 h-28"
-                    ref={provided.innerRef}
-                    {...provided.droppableProps}
-                  >
-                    {rows["Question"].items.map((item, index) => {
-                      return (
-                        <Draggable
-                          key={item.id}
-                          draggableId={item.id}
-                          index={index}
-                        >
-                          {(provided, snapshot) => {
-                            return <Option item={item} provided={provided} />;
-                          }}
-                        </Draggable>
-                      );
-                    })}
-                    {provided.placeholder}
-                  </div>
-                );
-              }}
-            </Droppable>
-          ) : null}
-
-          <div className="mt-10 mb-10 md:mt-5">
-            <Button
-              onClick={() => services.handleSubmit(rows, setRows, items)}
-              color="red"
-              buttonText="Prüfe dein Antwort"
+                    <div className="mt-10 mb-10 md:mt-5">
+                        <Button
+                            onClick={() => services.handleSubmit(rows, setRows, items)}
+                            color="red"
+                            buttonText="Prüfe dein Antwort"
+                        />
+                    </div>
+                </div>
+            </DragDropContext>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
             />
-          </div>
-        </div>
-      </DragDropContext>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
 
-      <div className="">
-        <Link to="/">
-          <Button
-            buttonText="Zurück zur Übersicht →"
-            className=""
-            color="lime"
-          />
-        </Link>
-      </div>
-    </main>
-  );
+            <div className="fixed bottom-0 left-0 w-full bg-cyan-200 flex justify-center p-4 md:relative md:p-0 md:bg-transparent">
+                <Link to="/">
+                    <Button buttonText="← Zurück" className="mr-4 mb-4" color="lime" />
+                </Link>
+                <Link to="chapter/1">
+                    <Button buttonText="Aufgabe 1 →" className="" color="red" />
+                </Link>
+            </div>
+        </main>
+    );
 };
 
 export default L1C2;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -61,7 +61,7 @@ const Home = () => {
       thumbnail={CardThumbnail}
       subtitle="Lektion 5"
       title="Thema der Lektion"
-      description="Explore additional examples in the GitHub repository."
+      description="ELorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. "
       callToActionText="Explore"
       calllToActionLink="/lesson/5"
     />,
@@ -121,44 +121,16 @@ const Home = () => {
               Lektion auswählen
             </h2>
             <div className="relative flex justify-center items-center">
-              <Button
-                onClick={handlePrevSlide}
-                rounded="full"
-                color="orange"
-                buttonText="&lt;"
-              />
-              <div className="w-full max-w-5xl overflow-hidden">
-                <div
-                  className="flex transition-transform duration-500 ease-in-out"
-                  style={{ transform: `translateX(-${currentSlide * 100}%)` }}
-                >
-                  {cards.map((card, index) => (
-                    <div
-                      key={index}
-                      className="w-full md:w-1/3 flex-shrink-0 p-4"
-                    >
-                      {card}
-                    </div>
-                  ))}
-                </div>
+              <div className="w-full grid grid-cols-1 md:grid-cols-3 gap-4">
+                {cards.map((card, index) => (
+                  <div
+                    key={index}
+                    className="w-full flex-shrink-0 p-4"
+                  >
+                    {card}
+                  </div>
+                ))}
               </div>
-              <Button
-                onClick={handleNextSlide}
-                rounded="full"
-                color="orange"
-                buttonText="&gt;"
-              />
-            </div>
-
-            <div className="flex justify-center mt-4">
-              {Array.from({ length: totalSlides }).map((_, index) => (
-                <span
-                  key={index}
-                  onClick={() => setCurrentSlide(index)}
-                  className={`mx-1 w-3 h-3 rounded-full cursor-pointer ${index === currentSlide ? "bg-black" : "bg-gray-400"
-                    }`}
-                ></span>
-              ))}
             </div>
           </div>
         </section>
