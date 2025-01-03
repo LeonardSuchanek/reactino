c3aa5aa39539306fd2611005bf58f22de8f62701
Leonard Suchanek <leonard.suchanek23091999@gmail.com>
2025-01-03 14:29:09 +0100

    toast in quiz

---
+++ Todo.md
@@ -0,0 +1,10 @@
+# ToDo Liste
+
+- Bilder für die Lektionen auf index.html
+- Verlinkung am Ende der Lektionen zu index.html
+- Zu jeder Lektion Quiz-Aufgaben (ChatGPT)
+
+# Optional
+
+- Scrollproblem
+-
--- src/components/lessons/lesson2/L2C1.tsx
+++ src/components/lessons/lesson2/L2C1.tsx
@@ -1,77 +1,118 @@
-import Quiz from 'react-quiz-component'
+import { useState } from "react";
+import Quiz from "react-quiz-component";
+import Toast, { ToastType } from "../../Toast";
+import Button from "../../Button";
 //https://github.com/wingkwong/react-quiz-component
 
-
 export const aufgabe = {
-  "quizSynopsis": "Testen Sie Ihr Wissen über die Grundlagen von Props und State in React. Viel Erfolg!",
-  "progressBarColor": "#9de1f6",
-  "nrOfQuestions": "3",
-  "questions": [
+  quizSynopsis:
+    "Testen Sie Ihr Wissen über die Grundlagen von Props und State in React. Viel Erfolg!",
+  progressBarColor: "#9de1f6",
+  nrOfQuestions: "3",
+  questions: [
     {
-      "question": "Was ist der Hauptunterschied zwischen Props und State in React?",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
+      question:
+        "Was ist der Hauptunterschied zwischen Props und State in React?",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
         "Props können verändert werden, State nicht.",
         "State kann verändert werden, Props nicht.",
         "Es gibt keinen Unterschied.",
-        "Beides wird nur für Dateninitialisierung verwendet."
+        "Beides wird nur für Dateninitialisierung verwendet.",
       ],
-      "correctAnswer": "2",
-      "messageForCorrectAnswer": "Richtige Antwort. Gut gemacht!",
-      "messageForIncorrectAnswer": "Falsche Antwort. Bitte versuchen Sie es erneut.",
-      "explanation": "Props sind unveränderlich (read-only), während State intern geändert werden kann.",
-      "point": "20"
+      correctAnswer: "2",
+      messageForCorrectAnswer: "Richtige Antwort. Gut gemacht!",
+      messageForIncorrectAnswer:
+        "Falsche Antwort. Bitte versuchen Sie es erneut.",
+      explanation:
+        "Props sind unveränderlich (read-only), während State intern geändert werden kann.",
+      point: "20",
     },
     {
-      "question": "Wie können Props an eine React-Komponente übergeben werden?",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
+      question: "Wie können Props an eine React-Komponente übergeben werden?",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: [
         "Als ein Argument in der `render()` Methode.",
         "Als ein Attribut in JSX.",
         "Durch Verwendung von `this.props`.",
-        "Durch den Import von `Props` aus React."
+        "Durch den Import von `Props` aus React.",
       ],
-      "correctAnswer": "2",
-      "messageForCorrectAnswer": "Richtige Antwort. Gut gemacht!",
-      "messageForIncorrectAnswer": "Falsche Antwort. Bitte versuchen Sie es erneut.",
-      "explanation": "Props werden an Komponenten über JSX-Attribute übergeben, z. B. `<Component propName={value} />`.",
-      "point": "20"
+      correctAnswer: "2",
+      messageForCorrectAnswer: "Richtige Antwort. Gut gemacht!",
+      messageForIncorrectAnswer:
+        "Falsche Antwort. Bitte versuchen Sie es erneut.",
+      explanation:
+        "Props werden an Komponenten über JSX-Attribute übergeben, z. B. `<Component propName={value} />`.",
+      point: "20",
     },
     {
-      "question": "Welches Hook wird verwendet, um den State in einer funktionalen Komponente zu verwalten?",
-      "questionType": "text",
-      "answerSelectionType": "single",
-      "answers": [
-        "useProps",
-        "useState",
-        "useEffect",
-        "useReducer"
-      ],
-      "correctAnswer": "2",
-      "messageForCorrectAnswer": "Richtige Antwort. Gut gemacht!",
-      "messageForIncorrectAnswer": "Falsche Antwort. Bitte versuchen Sie es erneut.",
-      "explanation": "`useState` ist ein React-Hook, der für die Verwaltung von lokalem State in funktionalen Komponenten verwendet wird.",
-      "point": "20"
-    }
-  ]
-}
-
+      question:
+        "Welches Hook wird verwendet, um den State in einer funktionalen Komponente zu verwalten?",
+      questionType: "text",
+      answerSelectionType: "single",
+      answers: ["useProps", "useState", "useEffect", "useReducer"],
+      correctAnswer: "2",
+      messageForCorrectAnswer: "Richtige Antwort. Gut gemacht!",
+      messageForIncorrectAnswer:
+        "Falsche Antwort. Bitte versuchen Sie es erneut.",
+      explanation:
+        "`useState` ist ein React-Hook, der für die Verwaltung von lokalem State in funktionalen Komponenten verwendet wird.",
+      point: "20",
+    },
+  ],
+};
 
 const L2C1 = () => {
+  const [toast, setToast] = useState<{
+    message: string;
+    type: ToastType;
+  } | null>(null);
+
+  const toastMessage = (quizResponse: any) => {
+    if (
+      quizResponse.numberOfIncorrectAnswers === quizResponse.numberOfQuestions
+    ) {
+      setToast({
+        message: `Du hast keine Frage richtig beantwortet. Bitte versuche es erneut.`,
+        type: "error",
+      });
+      return;
+    }
+    setToast({
+      message: `Du hast ${quizResponse.numberOfCorrectAnswers} von ${quizResponse.numberOfQuestions} Fragen richtig beantwortet.`,
+      type: "success",
+    });
+  };
+
   return (
     <main className="max-w-7xl md:justify-self-center">
       <div>
-
         <h1 className="font-bold text-4xl md:text-5xl mb-1">
           Quiz 1: Props und State
         </h1>
-        <p className="text-xl md:text-2xl mb-3">
-          Hier TEXT!
-        </p>
-        <Quiz quiz={aufgabe} showInstantFeedback={false} showDefaultResult={false} timer={60} continueTillCorrect={false} />
+        <p className="text-xl md:text-2xl mb-3">Hier TEXT!</p>
+        <Quiz
+          quiz={aufgabe}
+          showInstantFeedback={true}
+          showDefaultResult={false}
+          timer={60}
+          continueTillCorrect={false}
+          onComplete={toastMessage}
+        />
       </div>
+
+      {toast && (
+        <>
+          <Toast message={toast.message} type={toast.type} />
+          <Button
+            color={"lime"}
+            onClick={() => window.location.reload()}
+            buttonText={"Nochmal versuchen"}
+          />
+        </>
+      )}
     </main>
   );
 };
