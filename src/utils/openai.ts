ce8c125fb4941ef09f7cd12b975a942a4187e361
Lukas <79853178+halu1031@users.noreply.github.com>
2025-01-03 15:34:47 +0100

    input fot openAI Token

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = "https://api.openai.com/v1/chat/completions";
 const API_KEY = "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";
 
-export const fetchChatGPTResponse = async (messages: { role: string; content: string }[]) => {
+export const fetchChatGPTResponse = async (token: String, messages: { role: string; content: string }[]) => {
   try {
     const response = await axios.post(
       API_URL,
@@ -14,7 +14,7 @@ export const fetchChatGPTResponse = async (messages: { role: string; content: st
       {
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${API_KEY}`,
+          Authorization: `Bearer ${token}`,
         },
       }
     );
