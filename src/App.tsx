faa50e0fb20a4b51d33f973f27f5aecc41802d37
LeonardSuchanek <leonard.suchanek23091999@gmail.com>
2025-01-04 10:46:19 +0100

    scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import AppRouter from "./AppRouter";
 import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./utils/scrollToTop";
 
 function App() {
   return (
     <Router>
-      <AppRouter />
+      <ScrollToTop>
+        <AppRouter />
+      </ScrollToTop>
     </Router>
   );
 }
